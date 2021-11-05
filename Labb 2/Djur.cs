3e98c723eac1a605002af52f560642c0f9178007
Dan <dan-gustafsson@hotmail.com>
2021-11-05 11:25:04 +0100

    Add project files.

---
+++ Labb 2/Djur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb_2
+{
+    class Djur
+    {
+        private int _Eyes, _Legs;
+        private string _Color, _Sound;
+        bool _LiveInWater;
+        
+
+        public Djur(int Eyes, int Legs, string Color, string Sound, bool LiveInWater)
+            {
+            _Eyes = Eyes;
+            _Legs = Legs;
+            _Color = Color;
+            _Sound = Sound;
+            _LiveInWater = LiveInWater;
+            }
+
+    }
+}
